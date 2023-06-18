46a30863ae20426c0aaeb2aebf6810b60d785b8e
NMakarevich <nikoladze529@mail.ru>
2023-06-19 00:03:24 +0300

    fix: fix issue with datetime input in safari

--- src/app/components/input-date/input-date.component.ts
+++ src/app/components/input-date/input-date.component.ts
@@ -8,19 +8,28 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class InputDateComponent implements OnInit {
   date!: string;
 
+  minDate!: string;
+
+  isSafari = false;
+
   @Output('setDate') setDate: EventEmitter<string> = new EventEmitter<string>();
 
   @Output('toggleDateInput') toggleDateInput: EventEmitter<boolean> =
     new EventEmitter<boolean>();
 
-  selectDate(event: Event) {
-    this.date = (event.target as HTMLInputElement).value;
+  selectDate(event: any) {
+    if (this.isSafari && new Date(event.value) > new Date(this.minDate)) {
+      this.date = event.value;
+    } else this.date = (event.target as HTMLInputElement).value;
     localStorage.setItem('date', this.date);
     this.setDate.emit(this.date);
     this.toggleDateInput.emit(false);
   }
 
   ngOnInit() {
+    this.isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+
+    this.minDate = new Date().toISOString().slice(0, 16);
     this.date = localStorage.getItem('date') || '';
     localStorage.setItem('date', this.date);
     Promise.resolve().then(() => {
