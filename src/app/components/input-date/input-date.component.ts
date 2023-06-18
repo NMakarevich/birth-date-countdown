e776e916b56933739b0d71c16083b48410dcdc5b
NMakarevich <nikoladze529@mail.ru>
2023-06-18 17:44:40 +0300

    fix: fix eslint, fix date issue

--- src/app/components/input-date/input-date.component.ts
+++ src/app/components/input-date/input-date.component.ts
@@ -3,26 +3,28 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-input-date',
   templateUrl: './input-date.component.html',
-  styleUrls: ['./input-date.component.scss']
+  styleUrls: ['./input-date.component.scss'],
 })
 export class InputDateComponent implements OnInit {
   date!: string;
 
-  @Output('setDate') setDate: EventEmitter<string> = new EventEmitter<string>()
+  @Output('setDate') setDate: EventEmitter<string> = new EventEmitter<string>();
 
-  @Output('toggleDateInput') toggleDateInput: EventEmitter<boolean> = new EventEmitter<boolean>()
+  @Output('toggleDateInput') toggleDateInput: EventEmitter<boolean> =
+    new EventEmitter<boolean>();
 
   selectDate(event: Event) {
     this.date = (event.target as HTMLInputElement).value;
-    localStorage.setItem('date', this.date)
-    this.setDate.emit(this.date)
-    this.toggleDateInput.emit(false)
+    localStorage.setItem('date', this.date);
+    this.setDate.emit(this.date);
+    this.toggleDateInput.emit(false);
   }
 
   ngOnInit() {
-    this.date = localStorage.getItem('date') || new Date().toLocaleString()
+    this.date = localStorage.getItem('date') || '';
+    localStorage.setItem('date', this.date);
     Promise.resolve().then(() => {
       this.setDate.emit(this.date);
-    })
+    });
   }
 }
