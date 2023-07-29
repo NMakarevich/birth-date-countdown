f2952fb01095f21d6f18e48d866171f7cbc03805
NMakarevich <nikoladze529@mail.ru>
2023-07-29 10:36:12 +0300

    Auto-generated commit [ci skip]

--- src/app/utils/utils.ts
+++
@@ -1,5 +0,0 @@
-export default function toISOStringWithTZ(date: Date) {
-  const timezoneOffset = date.getTimezoneOffset() * 60 * 1000;
-  const time = date.getTime();
-  return new Date(time - timezoneOffset).toISOString().slice(0, 16);
-}
