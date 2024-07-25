98c6cce931893d0ab9fa0ca6b099f0080b12b33d
NMakarevich <nikoladze529@mail.ru>
2024-07-25 10:21:21 +0300

    fix: add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,12 @@
 import { ApplicationConfig } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { importProvidersFrom } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(ServiceWorkerModule.register('ngsw-worker.js')),
+  ],
 };
